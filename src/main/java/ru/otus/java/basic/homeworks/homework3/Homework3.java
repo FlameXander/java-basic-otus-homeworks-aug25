282960c1b738d7e8162c19d3879f10719b9578b4
Alexander Fisunov <fisunov.a.v@gmail.com>
2025-08-07 21:50:06 +0300

    Дз3 выполнено

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework3/Homework3.java
@@ -0,0 +1,9 @@
+package ru.otus.java.basic.homeworks.homework3;
+
+public class Homework3 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(3);
+        System.out.println(2);
+    }
+}
