4c0e73e43113f3257e79af65df260ba322c9dfcc
Alexander Fisunov <fisunov.a.v@gmail.com>
2025-08-07 21:52:13 +0300

    Дз3 доработано

--- src/main/java/ru/otus/java/basic/homeworks/homework3/Homework3.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework3/Homework3.java
@@ -3,7 +3,14 @@ package ru.otus.java.basic.homeworks.homework3;
 public class Homework3 {
     public static void main(String[] args) {
         System.out.println(1);
-        System.out.println(3);
         System.out.println(2);
+        System.out.println(3);
+    }
+
+    public static void addExample() {
+        int a = 10;
+        int b = 4;
+        int c = a + b;
+        System.out.println(c);
     }
 }
